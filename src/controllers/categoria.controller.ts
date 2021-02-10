2c2e4639a8690ef35e31a512e6ff153f78cb0cba
Gabriel Dias <dias.1618@gmail.com>
2021-02-09 21:07:28 -0300

    feature(categoria): Adicionado insert e update

--- src/controllers/categoria.controller.ts
+++ src/controllers/categoria.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, HttpCode } from "@nestjs/common";
+import { Controller, Get, HttpCode, Post, Body, Put } from "@nestjs/common";
 import { CategoriaService } from "src/services/categoria.service";
+import { Categoria } from "src/entities/categoria.entity";
 
 @Controller('categorias')
 export class CategoriaController {
@@ -7,11 +8,21 @@ export class CategoriaController {
       private readonly categoriaService: CategoriaService,
   ) {}
 
-
   @Get()
   @HttpCode(200)
   async get() {
     return await this.categoriaService.get();
   }
 
+  @Post()
+  @HttpCode(200)
+  async insert(@Body() categoria:Categoria) {
+      return await this.categoriaService.save(categoria);
+  }
+
+  @Put()
+  @HttpCode(200)
+  async update(@Body() categoria:Categoria) {
+      return await this.categoriaService.save(categoria);
+  }
 }
